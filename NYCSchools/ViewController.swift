21baadf44ddcbfb595fe5317420e8c0287b7c42f
ivan <tellezivan7@gmail.com>
2023-06-29 21:00:04 -0700

    Initial Commit

---
+++ NYCSchools/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NYCSchools
+//
+//  Created by Telles Ivan on 6/29/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
