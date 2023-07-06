18be1433ba44f26fc73e8892b7a2a96d9847ae36
ivan <tellezivan7@gmail.com>
2023-07-05 18:31:59 -0700

    feat: add unit testing practice

---
+++ NYCSchools/App/DataLayer/Models/SchoolListLoadState.swift
@@ -0,0 +1,12 @@
+//
+//  SchoolListState.swift
+//  NYCSchools
+//
+//  Created by Telles Ivan on 7/5/23.
+//
+
+import Foundation
+
+enum SchoolListState {
+    
+}
---
+++ NYCSchoolsTests/MockSchoolApi.swift
@@ -0,0 +1,8 @@
+//
+//  MockSchoolApi.swift
+//  NYCSchoolsTests
+//
+//  Created by Telles Ivan on 7/5/23.
+//
+
+import Foundation
